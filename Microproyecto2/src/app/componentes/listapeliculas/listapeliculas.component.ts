d39f9b683f99c84a09326ea710414ddb42587d6e
Norangel Marin <norangel.marin@correo.unimet.edu.ve>
2021-03-08 04:05:34 -0400

    uso de dos endpoints

--- Microproyecto2/src/app/componentes/listapeliculas/listapeliculas.component.ts
+++ Microproyecto2/src/app/componentes/listapeliculas/listapeliculas.component.ts
@@ -18,7 +18,7 @@ export class ListapeliculasComponent implements OnInit {
 
   public cargardata(){
     this.ApiService.Obtener("https://api.themoviedb.org/3/discover/movie?api_key=45de787fddd0a31b33ab5815e5b2ef85&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1").subscribe(respuesta =>{
-      for (let index = 0; index < 8; index++) {
+      for (let index = 0; index < respuesta['results'].length ; index++) {
       const data = respuesta['results'][index]
       this.listapeliculas[index] = data
       } 
--- Microproyecto2/src/app/componentes/peliculas/peliculas.component.ts
+++ Microproyecto2/src/app/componentes/peliculas/peliculas.component.ts
@@ -17,7 +17,7 @@ export class PeliculasComponent implements OnInit {
   }
 
   public cargardata(){
-    this.ApiService.Obtener("https://api.themoviedb.org/3/discover/movie?api_key=45de787fddd0a31b33ab5815e5b2ef85&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1").subscribe(respuesta =>{
+    this.ApiService.Obtener('https://api.themoviedb.org/3/movie/upcoming?api_key=45de787fddd0a31b33ab5815e5b2ef85&language=en-US&page=1').subscribe(respuesta =>{
       for (let index = 0; index < 8; index++) {
       const data = respuesta['results'][index]
       this.tarjetas[index] = data
